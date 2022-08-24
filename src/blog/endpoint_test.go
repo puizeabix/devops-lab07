6d39f877d688904f4eb11a3bd1bfee5a77b1fc8f
Prawit Chaivong <prawit.c@zeabix.com>
2022-08-24 11:09:10 +0700

    Add simple unittest

---
+++ src/blog/endpoint_test.go
@@ -0,0 +1,30 @@
+package blog
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestPublishBlogResponseError(t *testing.T) {
+	expected := errors.New("Test")
+	p := publishBlogResponse{
+		Err: expected,
+	}
+
+	result := p.error()
+	if result != expected {
+		t.Errorf("got %v, expected %v", result, expected)
+	}
+}
+
+func TestListBlogResponseError(t *testing.T) {
+	expected := errors.New("Test")
+	l := listBlogResponse{
+		Err: expected,
+	}
+
+	result := l.error()
+	if result != expected {
+		t.Errorf("got %v, expected %v", result, expected)
+	}
+}
