d158cd57e69e4cf17374894ae88d902e0c38a07b
Prawit Chaivong <prawit.c@zeabix.com>
2022-08-24 23:58:47 +0700

    Upgrade validate script

--- validate.py
+++ validate.py
@@ -3,11 +3,11 @@ import os
 token = os.environ['TOKEN']
 print(f'The token retrieved is {token}')
 
-if token == "production":
+if token == "aaa":
     print("The TOKEN retrieved from production environment")
-elif token == "dev":
+elif token == "bbb":
     print("The TOKEN retrieved from dev environment")
-elif token == "repo":
+elif token == "ccc":
     print("The TOKEN retrieved from repo secrets")
 else:
     print("The TOKEN doesn't exist")
